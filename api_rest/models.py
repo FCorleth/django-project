3b82d19e6eed21b2e82a01c801e0715dada846f7
Fábio Corleth <fabiocorleth@Fabios-MacBook-Air.local>
2026-06-30 20:50:52 -0300

    feat: create `User` model

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(primary_key=True, max_length=100, default='')
+    name = models.CharField(max_length=150, default='')
+    email = models.EmailField(default='')
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Nickname: {self.nickname}. Email: {self.name}'
